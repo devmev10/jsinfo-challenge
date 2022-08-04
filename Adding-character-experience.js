2ff98676102db4b0f25b648725f2b47f524a85a6
devmev10 <ravitokas90@gmail.com>
2022-08-04 11:47:23 +0530

    Complete 'Adding-character-experience' JS challenge

---
+++ Adding-character-experience.js
@@ -0,0 +1,25 @@
+//Improve our example RPG program to add an experience property named xp to the character. 
+//Its initial value is 0. Experience must appear in character description
+// TODO: create the character object here
+const aurora = {
+    name: "Aurora",
+    health: 150,
+    strength: 25,
+    xp: 0,
+    // Return the character description
+    describe() {
+      return `${this.name} has ${this.health} health points, ${this
+        .strength} as strength and ${this.xp} XP points`;
+    }
+  };
+
+// Aurora is harmed by an arrow
+aurora.health -= 20;
+
+// Aurora equips a strength necklace
+aurora.strength += 10;
+
+// Aurora learn a new skill
+aurora.xp += 15;
+
+console.log(aurora.describe());
