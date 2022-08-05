1c56dd84640db2a97b4ce3bb3fbb0e6911ad11e9
devmev10 <ravitokas90@gmail.com>
2022-08-05 11:49:07 +0530

    Complete 'bank' JS challenge

---
+++ bank.js
@@ -0,0 +1,15 @@
+// Write a program that creates an account object with the following characteristics:
+
+// A name property set to "Alex".
+// A balance property set to 0.
+// A credit method adding the (positive or negative) value passed as an argument to the account balance.
+// A describe method returning the account description.
+// Use this object to show its description, crediting 250, debiting 80, then show its description again.
+let account = {
+    name: 'Alex',
+    balance: 0,
+    credit(){return this.name += this.balance},
+    describe(){return `owner: ${this.name}, balance: ${this.balance}`},
+}
+console.log(account.describe())
+console.log(account.describe())
