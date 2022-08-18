18553e69ea2005fbb96f44250fb980ca457d73b1
devmev10 <ravitokas90@gmail.com>
2022-08-18 11:58:24 +0530

    Complete 'Sort in decreasing order' JS challenge

---
+++ Sort in decreasing order.js
@@ -0,0 +1,11 @@
+// Sort in decreasing order
+// importance: 4
+// let arr = [5, 2, 1, -10, 8];
+
+// // ... your code to sort it in decreasing order
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+let arr = [5, 2, 1, -10, 8]
+arr.sort((a,b) => b-a)
+alert(arr)
